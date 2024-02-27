bfd8c9945267d69873a88b780ea55d04e7091b19
dongapple <hg971027@gmail.com>
2024-02-27 18:21:52 +0900

    🌊 컬러 임포트문 경로 수정 #2

--- src/components/InputDaySmall.tsx
+++ src/components/InputDaySmall.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import '@/themes/typography.css';
-import * as colors from '@/themes/color.js';
+import * as colors from '@/themes/color';
 
 interface InputDaySmallProps {
   ischecked: boolean;
