86f62e246ef75a1876b4f1152cf567934f1d24b6
dongapple <hg971027@gmail.com>
2024-03-04 16:09:52 +0900

    ✨ 삭제 및 저장 버튼 추가 #11
    - 내부를 폼으로 감싸고 submit 과 button 를 추가
    - 하나의 styled 컴포넌트로 props 값을 받아 관리

--- src/components/ModalSetting.tsx
+++ src/components/ModalSetting.tsx
@@ -15,17 +15,18 @@ const StyledSection = styled.section`
 
   margin: 0 1rem 0 1rem;
   padding: 0 1rem 0 1rem;
-  // background-color: red;
+  // background-color: ${colors.primary500};
+`;
+
+const StyledForm = styled.form`
+  /* 여기에 폼의 스타일을 추가하세요 */
 `;
 
 const StyledUl = styled.ul`
   display: flex;
   flex-direction: column;
   gap: 1rem;
-  // background-color: blue;
 `;
-
-const StyledLi = styled.li``;
 
 const StyledH3 = styled.h3`
   padding: 0.5rem 1rem;
@@ -45,6 +46,14 @@ const StyledDiv = styled.div<{ column?: boolean; gap?: string }>`
 
   color: ${colors.black};
   background-color: ${colors.primary100};
+`;
+
+const StyledButtonsContainer = styled.div`
+  position: fixed;
+  bottom: 0px;
+  left: 1rem;
+
+  width: calc(100% - 2rem);
 `;
 
 const StyledSearch = styled.input`
@@ -85,6 +94,15 @@ const StyledChevronBottom = styled.button`
   border: none;
 `;
 
+const StyledButton = styled.button<{ type?: string }>`
+  width: 50%;
+  padding: 0.5rem 1rem;
+  border: none;
+  background-color: ${(props) =>
+    props.type === 'submit' ? colors.primary900 : colors.primary100};
+  color: ${(props) => (props.type === 'submit' ? colors.white : colors.red)};
+`;
+
 const StyledWarning = styled.p`
   padding-top: 0.25rem;
   color: ${colors.red};
@@ -97,60 +115,83 @@ const ModalSetting: React.FC<ModalSettingProps> = ({}) => {
         <StyledChevronBottom>
           <img src={chevronBottom} alt="" />
         </StyledChevronBottom>
+        <StyledForm>
+          <StyledUl>
+            <li>
+              <StyledSearch
+                type="search"
+                placeholder="버스 정류장 이름을 입력해 주세요"
+                className="body4"
+              />
+            </li>
 
-        <StyledUl>
-          <li>
-            <StyledSearch
-              type="search"
-              placeholder="버스 정류장 이름을 입력해 주세요"
-              className="body4"
-            />
-          </li>
+            <li>
+              <StyledH3 className="title3">버스 정류장 이름</StyledH3>
+              <StyledDiv column>
+                <InputCheck ischecked={false} text="8-1" onClick={() => {}} />
+              </StyledDiv>
+              <StyledWarning className="body7">
+                최소 1개 이상의 버스를 선택해 주세요.
+              </StyledWarning>
+            </li>
 
-          <li>
-            <StyledH3 className="title3">버스 정류장 이름</StyledH3>
-            <StyledDiv column>
-              <InputCheck ischecked={false} text="8-1" onClick={() => {}} />
-            </StyledDiv>
-            <StyledWarning className="body7">
-              주의 문구를 입력해 주세요
-            </StyledWarning>
-          </li>
+            <li>
+              <StyledH3 className="title3">버스 탑승시각</StyledH3>
+              <StyledDiv>
+                <StyledTime className="body4" type="time" />
+              </StyledDiv>
+              <StyledWarning className="body7">
+                원하는 버스 탑승시각을 설정해 주세요.
+              </StyledWarning>
+            </li>
 
-          <li>
-            <StyledH3 className="title3">버스 탑승시각</StyledH3>
-            <StyledDiv>
-              <StyledTime className="body4" type="time" />
-            </StyledDiv>
-            <StyledWarning className="body7">
-              주의 문구를 입력해 주세요
-            </StyledWarning>
-          </li>
+            <li>
+              <StyledH3 className="title3">이동 소요시간</StyledH3>
+              <StyledDiv>
+                <StyledNumber
+                  className="body4"
+                  type="number"
+                  min={0}
+                  max={10}
+                />
+              </StyledDiv>
+              <StyledWarning className="body7">
+                출발지부터 정류장까지 소요 시간을 입력해 주세요. ( 최대 10분 )
+              </StyledWarning>
+            </li>
 
-          <li>
-            <StyledH3 className="title3">이동 소요시간</StyledH3>
-            <StyledDiv>
-              <StyledNumber className="body4" type="number" min={0} max={10} />
-            </StyledDiv>
-          </li>
+            <li>
+              <StyledH3 className="title3">알람 반복</StyledH3>
+              <StyledDiv>
+                <InputDaySmall ischecked={false} text="월" onClick={() => {}} />
+                <InputDaySmall ischecked={false} text="월" onClick={() => {}} />
+                <InputDaySmall ischecked={false} text="월" onClick={() => {}} />
+              </StyledDiv>
+              <StyledWarning className="body7">
+                미선택 시 알람이 한 번만 울립니다.
+              </StyledWarning>
+            </li>
 
-          <li>
-            <StyledH3 className="title3">알림 주기</StyledH3>
-            <StyledDiv>
-              <InputDaySmall ischecked={false} text="월" onClick={() => {}} />
-              <InputDaySmall ischecked={false} text="월" onClick={() => {}} />
-              <InputDaySmall ischecked={false} text="월" onClick={() => {}} />
-            </StyledDiv>
-          </li>
-
-          <li>
-            <StyledH3 className="title3">알람 간격</StyledH3>
-            <StyledDiv>
-              <InputCheck ischecked={false} text="직전" onClick={() => {}} />
-              <InputCheck ischecked={false} text="5분전" onClick={() => {}} />
-            </StyledDiv>
-          </li>
-        </StyledUl>
+            <li>
+              <StyledH3 className="title3">알람 간격</StyledH3>
+              <StyledDiv>
+                <InputCheck ischecked={false} text="직전" onClick={() => {}} />
+                <InputCheck ischecked={false} text="5분전" onClick={() => {}} />
+              </StyledDiv>
+              <StyledWarning className="body7">
+                선택된 시간에 알람이 울림니다.
+              </StyledWarning>
+            </li>
+          </StyledUl>
+          <StyledButtonsContainer>
+            <StyledButton className="title3" type="button">
+              삭제
+            </StyledButton>
+            <StyledButton className="title3" type="submit">
+              저장
+            </StyledButton>
+          </StyledButtonsContainer>
+        </StyledForm>
       </StyledSection>
     </>
   );
